1c0f1eda93987e3133d157a395a7e55cd928f556
Jatin Patel <jatinpatel136@gmail.com>
2021-03-13 22:41:57 +0530

    code for CRUD functionality

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 //mongodb+srv://gacs:<password>@cluster0.lxlyv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const connectDB = async () => {
+    console.log(process.env.MONGO_URI)
     const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
 
     console.log(`Mongodb connected: ${conn.connection.host}`)
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-const connectDB = require('./config/db');
 const dotenv = require('dotenv');
-const cors = require('cors')
+const cors = require('cors');
+const Employee = require('./models/Employee');
+const connectDB = require('./config/db');
 
 // Load env files
 dotenv.config({ path: './config/config.env' });
@@ -20,12 +21,54 @@ app.get('/', (req, res) => {
     res.send('Root path')
 });
 
+app.post('/employees', async (req, res, next) => {
+    const employee = await Employee.create(req.body);
+    res.status(201).json({ success: true, data: employee });
+})
+
+app.get('/employees', async (req, res, next) => {
+    const employees = await Employee.find();
+    res.status(200).json({ success: true, data: employees });
+})
+
+app.get('/employees/:id', async (req, res, next) => {
+    const employee = await Employee.findById(req.params.id);
+    if (!employee) {
+        // This is error when Object id is properly formatted but not found in Database
+        return next(new ErrorResponse(`Employee with ${req.params.id} not found`, 404));
+    }
+    res.status(200).json({ success: true, data: employee });
+});
+
+app.put('/employees/:id', async (req, res, next) => {
+    let employee = await Employee.findById(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    if (!employee) {
+        return next(new ErrorResponse(`Employee with ${req.params.id} not found`, 404));
+    }
+
+    employee = await Employee.findOneAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({ success: true, data: employee });
+});
+
+app.delete('/employees/:id', async (req, res, next) => {
+    const employee = await Employee.findById(req.params.id);
+
+    if (!employee) {
+        return next(new ErrorResponse(`Employee with ${req.params.id} not found`, 404));
+    }
+
+    employee.remove();
+
+    res.status(200).json({ success: true, data: {} });
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`server running on ${PORT}`));
-
-// Handle unhandled promise rejection
-process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`.red);
-    // close server and exit
-    server.close(() => process.exit(1));
-})
