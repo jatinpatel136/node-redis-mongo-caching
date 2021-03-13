108465cad0b7e0fc58162292c0413f8f69ea2b10
Jatin Prakash Patel <patel-j89@acs.uwinnipeg.ca>
2021-03-13 08:55:30 -0600

    added .env file and sample json data

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+const cors = require('cors')
 
+// Load env files
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Body Parser
+app.use(express.json())
+
+// Enable CORS
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Root path')
@@ -8,3 +22,10 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`server running on ${PORT}`));
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // close server and exit
+    server.close(() => process.exit(1));
+})
